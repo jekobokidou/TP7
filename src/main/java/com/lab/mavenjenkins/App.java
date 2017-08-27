ecd9283f8efbb2206ac94ac42fddb000e7971d98
Joseph EKOBO KIDOU <DGKX7376@orange.com>
2017-08-27 17:12:48 +0200

    initial commit

---
+++ src/main/java/com/lab/mavenjenkins/App.java
@@ -0,0 +1,13 @@
+package com.lab.mavenjenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
